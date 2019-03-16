38045180feb096e8c2b8292782cb05971d5634e9
vagrant <naojirouhisada@gmail.com>
2019-03-16 06:56:37 +0000

    ヘッダーレイアウトの作成

--- work/gulp/config.js
+++ work/gulp/config.js
@@ -3,4 +3,5 @@ module.exports = {
   build: './build', //出力先ディレクトリ
   src: './src', //ソースディレクトリ
   scss: './assets/scss', //scssファイルを格納するディレクトリ
+  sprite: './assets/sprite' //スプライト化する画像を管理するディレクトリ
 }
